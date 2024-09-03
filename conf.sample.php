444be4fdb35b666957c726f86335da069b05de5d
zygoballus <119077481+zygoballus@users.noreply.github.com>
2024-09-02 22:27:59 -0400

    conf.sample.php instructions

--- conf.sample.php
+++ conf.sample.php
@@ -1,8 +1,11 @@
 <?php
 // Get these values from https://www.inaturalist.org/oauth/applications.
-// Then rename this file to conf.php.
 $app_id = '';
 $app_secret = '';
 $redirect_uri = '';
-$username = ''; // Your iNaturalist username
-$password = ''; // Your iNaturalist password
+// You should specify a default iNaturalist username and password here. If they
+// are left blank, you will need to enter a username and password in the form
+// each time in order to authenticate.
+$username = '';
+$password = '';
+// Finally, rename this file to conf.php.
