30c0d1f44e3aa29ec244abef68393a4e5f9e0c61
zygoballus <119077481+zygoballus@users.noreply.github.com>
2024-10-02 21:10:34 -0400

    Moving DNA grabber to new repo

--- db.conf.sample.php
+++
@@ -1,7 +0,0 @@
-<?php
-// To use this file, create a copy named db.conf.php and fill in the appropriate values.
-$dbname = "";
-$server = "";
-$user = "username";
-$pass = "password";
-$link = mysqli_connect($server, $user, $pass, $dbname);
